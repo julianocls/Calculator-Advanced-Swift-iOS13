d3cf9848e7db9b8ff5865cee3a61e72e660e7406
Juliano C L Santos <foxports@hotmail.com>
2023-01-29 02:00:57 -0300

    operation basic implement

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,20 +12,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
-    
+        if let safeSymbol = sender.currentTitle {
+            if safeSymbol == "+/-" {
+                displayLabel.text = displayLabel.text?.first == "-" ? displayLabel.text?.replacingOccurrences(of: "-", with: "") : "-\(displayLabel.text ?? "0")"
+            } else {
+                isFinishedTypingNumber = true
+                if safeSymbol == "AC" {
+                    displayLabel.text = "0"
+                }
+                
+            }
+        }
     }
-
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
-    
+        if let safeNum = sender.currentTitle {
+            if isFinishedTypingNumber && safeNum != "." {
+                displayLabel.text = safeNum
+                isFinishedTypingNumber = false
+            } else {
+                if !(safeNum == "." && havePoint()) {
+                    displayLabel.text?.append(safeNum)
+                }
+            }
+        }
     }
-
+    
+    fileprivate func havePoint() -> Bool {
+        return displayLabel.text?.firstIndex(of: ".") != nil
+    }
 }
 
